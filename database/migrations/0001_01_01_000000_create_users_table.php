b4c0f1638b8db418bb86c459df9150b295b02901
Tu X. Thinh <135200068+XuanThinhTu@users.noreply.github.com>
2024-09-18 14:48:26 +0700

    Update 0001_01_01_000000_create_users_table.php
    clear error

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -20,7 +20,6 @@ return new class extends Migration
             $table->string('address')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('address');
             $table->rememberToken();
             $table->timestamps();
         });
